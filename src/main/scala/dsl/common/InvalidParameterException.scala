9ef529b5f3c22d25c789fdc1d757395a40f49d65
Guillermina Cledou <guillecledou@gmail.com>
2019-07-02 11:40:44 +0100

    Adding missing files.

---
+++ src/main/scala/dsl/common/InvalidParameterException.scala
@@ -0,0 +1,9 @@
+package dsl.common
+
+/**
+  * Created by guillerminacledou on 2019-07-01
+  */
+
+
+class InvalidParameterException(err:String) extends RuntimeException(err) {
+}
---
+++ src/main/scala/dsl/common/PatternMatchingException.scala
@@ -0,0 +1,8 @@
+package dsl.common
+
+/**
+  * Created by guillecledou on 2019-07-02
+  */
+
+
+class PatternMatchingException(err:String) extends RuntimeException(err) {}
